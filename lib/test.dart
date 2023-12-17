c49c5d1a93e3f2f54a28ba1d3a143b64d5becde6
yamazakimina <130649486+yamazakimina@users.noreply.github.com>
2023-12-17 11:54:03 +0900

    [fix]テストデータの修正とpubspec.yamlへの追加

--- lib/test.dart
+++ lib/test.dart
@@ -17,7 +17,23 @@ List<Map<String, dynamic>> testUserData = [
   },
   {
     'name': 'Alice',
-    'pictures': ['assets/img/1.jpg', 'assets/img/2.jpg', 'assets/img/3.jpg'],
+    'pictures': ['assets/img/2.jpg', 'assets/img/1.jpg', 'assets/img/3.jpg'],
+    'videos': [
+      'assets/video/1.mp4',
+      'assets/video/1.mp4',
+      'assets/video/1.mp4'
+    ],
+    'age': '25',
+    'city': 'Tokyo',
+    'country': 'Japan',
+    'gender': 'woman',
+    'state': 'test',
+    'phone': '09088881111',
+    'email': 'test@gmail.com'
+  },
+  {
+    'name': 'Alice',
+    'pictures': ['assets/img/3.jpg', 'assets/img/2.jpg', 'assets/img/1.jpg'],
     'videos': [
       'assets/video/1.mp4',
       'assets/video/1.mp4',
@@ -49,7 +65,23 @@ List<Map<String, dynamic>> testUserData = [
   },
   {
     'name': 'Alice',
-    'pictures': ['assets/img/1.jpg', 'assets/img/2.jpg', 'assets/img/3.jpg'],
+    'pictures': ['assets/img/2.jpg', 'assets/img/1.jpg', 'assets/img/3.jpg'],
+    'videos': [
+      'assets/video/1.mp4',
+      'assets/video/1.mp4',
+      'assets/video/1.mp4'
+    ],
+    'age': '25',
+    'city': 'Tokyo',
+    'country': 'Japan',
+    'gender': 'woman',
+    'state': 'test',
+    'phone': '09088881111',
+    'email': 'test@gmail.com'
+  },
+  {
+    'name': 'Alice',
+    'pictures': ['assets/img/3.jpg', 'assets/img/2.jpg', 'assets/img/1.jpg'],
     'videos': [
       'assets/video/1.mp4',
       'assets/video/1.mp4',
@@ -81,7 +113,7 @@ List<Map<String, dynamic>> testUserData = [
   },
   {
     'name': 'Alice',
-    'pictures': ['assets/img/1.jpg', 'assets/img/2.jpg', 'assets/img/3.jpg'],
+    'pictures': ['assets/img/2.jpg', 'assets/img/1.jpg', 'assets/img/3.jpg'],
     'videos': [
       'assets/video/1.mp4',
       'assets/video/1.mp4',
@@ -97,55 +129,7 @@ List<Map<String, dynamic>> testUserData = [
   },
   {
     'name': 'Alice',
-    'pictures': ['assets/img/1.jpg', 'assets/img/2.jpg', 'assets/img/3.jpg'],
-    'videos': [
-      'assets/video/1.mp4',
-      'assets/video/1.mp4',
-      'assets/video/1.mp4'
-    ],
-    'age': '25',
-    'city': 'Tokyo',
-    'country': 'Japan',
-    'gender': 'woman',
-    'state': 'test',
-    'phone': '09088881111',
-    'email': 'test@gmail.com'
-  },
-  {
-    'name': 'Alice',
-    'pictures': ['assets/img/1.jpg', 'assets/img/2.jpg', 'assets/img/3.jpg'],
-    'videos': [
-      'assets/video/1.mp4',
-      'assets/video/1.mp4',
-      'assets/video/1.mp4'
-    ],
-    'age': '25',
-    'city': 'Tokyo',
-    'country': 'Japan',
-    'gender': 'woman',
-    'state': 'test',
-    'phone': '09088881111',
-    'email': 'test@gmail.com'
-  },
-  {
-    'name': 'Alice',
-    'pictures': ['assets/img/1.jpg', 'assets/img/2.jpg', 'assets/img/3.jpg'],
-    'videos': [
-      'assets/video/1.mp4',
-      'assets/video/1.mp4',
-      'assets/video/1.mp4'
-    ],
-    'age': '25',
-    'city': 'Tokyo',
-    'country': 'Japan',
-    'gender': 'woman',
-    'state': 'test',
-    'phone': '09088881111',
-    'email': 'test@gmail.com'
-  },
-  {
-    'name': 'Alice',
-    'pictures': ['assets/img/1.jpg', 'assets/img/2.jpg', 'assets/img/3.jpg'],
+    'pictures': ['assets/img/3.jpg', 'assets/img/2.jpg', 'assets/img/1.jpg'],
     'videos': [
       'assets/video/1.mp4',
       'assets/video/1.mp4',
